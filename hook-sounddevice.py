d05992e5805c2b1077badb91b3e37e4d56c64aa2
yuvmirk <yuvmirk@gmail.com>
2024-08-27 08:57:31 +0300

    updated hidden dependencies

--- hook-sounddevice.py
+++ hook-sounddevice.py
@@ -1,4 +1,4 @@
-from PyInstaller.utils.hooks import collect_dynamic_libs
+from PyInstaller.utils.hooks import collect_dynamic_libs, collect_submodules
 
 binaries = collect_dynamic_libs('sounddevice')
 hiddenimports = collect_submodules('sounddevice')
